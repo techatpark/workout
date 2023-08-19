669e95846ee01aaa035d104a2c43067b2a391288
haripriya <rm.haripriya@gmail.com>
2023-08-19 16:26:08 +0530

    Check

--- browser-sync.js
+++ browser-sync.js
@@ -14,6 +14,6 @@
 module.exports = {
     files: ["dist/css/*.css", "dist/js/*.js", "dist/**/*.html"],
     serveStatic: ['dist'],
-    proxy: "https://workout.techatpark.com",
+    proxy: "http://localhost:8080",
     ghostMode: false,
 };
